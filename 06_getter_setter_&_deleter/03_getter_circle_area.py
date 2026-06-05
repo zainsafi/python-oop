25e446068637aff8383b31b0dc3d06d57a1ea271
zainsafi <zainulislamk456@gmail.com>
2026-06-05 08:21:06 +0500

    add Circle class with radius and area properties using getters

---
+++ 06_getter_setter_&_deleter/03_getter_circle_area.py
@@ -0,0 +1,16 @@
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self): # A getter to get the radius
+        return self._radius
+  
+    @property
+    def area(self):  # A getter to calculate area
+        return 3.14 * (self._radius ** 2)
+
+my_circle = Circle(3)
+
+print(my_circle.radius) # 3
+print(my_circle.area) # 28.26
