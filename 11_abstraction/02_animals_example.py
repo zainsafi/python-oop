2b9019474e8041686a3e22e9de1fc8835498a78b
zainsafi <zainulislamk456@gmail.com>
2026-06-20 08:21:35 +0500

    add abstract class implementation with Animal, Dog, Cat, and Monkey examples

---
+++ 11_abstraction/02_animals_example.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC): # Inherits from abstract base class
+   @abstractmethod # Abstract method decorator
+   def make_sound(self):  # The method subclasses must override
+       pass
+
+# Concrete class that will override the abstract method
+class Dog(Animal):
+   def make_sound(self):
+       print('Woof!')
+
+# Another concrete class that will override the abstract method
+class Cat(Animal):
+   def make_sound(self):
+       print('Meow!')
+
+# Another concrete class that will override the abstract method
+class Monkey(Animal):
+   def make_sound(self):
+       print('Ooh ooh aah aah!')
+
+# Create instances of each concrete class
+animals = [Dog(), Cat(), Monkey()]
+
+# Loop through the instances to call the make_sound method
+for animal in animals:
+   animal.make_sound()
+
+# Output:
+# Woof!
+# Meow!
+# Ooh ooh aah aah!
